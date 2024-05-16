3d9bd7a422e514e684ec5cb56a8c46c255363299
hoquocnhat1506 <89431816+hoquocnhat1506@users.noreply.github.com>
2024-05-16 15:46:57 +0700

    fix map

--- src/components/DeviceMap/index.tsx
+++ src/components/DeviceMap/index.tsx
@@ -4,8 +4,6 @@ import "leaflet/dist/leaflet.css";
 import L, { LatLngExpression, LeafletMouseEvent } from "leaflet";
 import "leaflet-search/dist/leaflet-search.min.css";
 import "leaflet-search";
-import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import iconLocation from "../../assets/svg/iconLocation.png";
 
 const DeviceMap: React.FC = () => {
@@ -31,47 +29,6 @@ const DeviceMap: React.FC = () => {
         }).addTo(map!);
 
         L.control.zoom({ position: "topright" }).addTo(map!);
-
-        // const geocoder = Control.Geocoder.nominatim();
-        // const geocoderControl = Control.geocoder({
-        //   geocoder: geocoder,
-        //   collapsed: false,
-        //   placeholder: "Search for a location...",
-        // }).addTo(map!);
-
-        // geocoderControl.on("markgeocode", function (event: any) {
-        //   const { center, name, properties } = event.geocode;
-
-        //   if (newMarkerRef.current) {
-        //     map?.removeLayer(newMarkerRef.current);
-        //   }
-
-        //   const newMarkerIcon = L.icon({
-        //     iconUrl: "http://maps.google.com/mapfiles/ms/micons/blue-dot.png",
-        //     iconSize: [32, 32],
-        //     iconAnchor: [16, 32],
-        //   });
-
-        //   const newMarker = L.marker(center, { icon: newMarkerIcon }).addTo(
-        //     map!
-        //   );
-        //   newMarkerRef.current = newMarker;
-
-        //   const { city, state, country } = properties;
-        //   const locationInfo = `<b>Name:</b> ${name}<br><b>City:</b> ${
-        //     city || "N/A"
-        //   }<br><b>State:</b> ${state || "N/A"}<br><b>Country:</b> ${
-        //     country || "N/A"
-        //   }`;
-
-        //   if (locationInfoRef.current) {
-        //     locationInfoRef.current.innerHTML = locationInfo;
-        //     const iconLocationElement = document.createElement("img");
-        //     iconLocationElement.src = iconLocation;
-        //     iconLocationElement.alt = "Location Icon";
-        //     locationInfoRef.current.appendChild(iconLocationElement);
-        //   }
-        // });
 
         map.on("click", (event: LeafletMouseEvent) => {
           const latlng = event.latlng;
